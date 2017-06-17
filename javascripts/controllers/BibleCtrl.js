4ec3a45695b627503d1d52ad47a454d93f8b19b4
bryanhmiller <bryankalos@hotmail.com>
2017-06-17 14:11:05 -0500

    Working on dropdowns for Bible search, have book and working on chapters

--- javascripts/controllers/BibleCtrl.js
+++ javascripts/controllers/BibleCtrl.js
@@ -3,7 +3,17 @@ app.controller("BibleCtrl", function($rootScope, $scope, BibleFactory) {
 	console.log("inside BibleCtrl.js");
 
   let loadBible = () => {
-    BibleFactory.getBible();
+    BibleFactory.getBible()
+    .then((books) => {
+    	$scope.books = books;
+    })
+    .catch((error) => {
+    	console.log("error in loadBible", error);
+    })
+  };
+
+  $scope.setSelectedBook = (book) => {
+  	
   };
 
   loadBible();
--- javascripts/factories/BibleFactory.js
+++ javascripts/factories/BibleFactory.js
@@ -1,20 +1,12 @@
-app.factory("BibleFactory", function($http, $q, apiBible, FIREBASE_CONFIG) {
+app.factory("BibleFactory", function($http, $q) {
 
   let getBible = () => {
+    let books = [];
     return $q((resolve, reject) => {
-      // let auth = $base64.encode('');
-      // let headers = {'Authorization': 'Basic ' + auth};
-      let auth = 'username=&grant_type=password';
-      $http({
-        method:'GET',
-        url:'https://bibles.org/v2/versions/eng-KJVA/books.js?include_chapters=true',
-        headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-        data:auth
-      })
-      .then((chapters) => {
-          resolve(chapters);
+      $http.get(`http://dbt.io/library/book?key=c0c769e931f78307a6c1c65cc5bd1d8c&dam_id=ENGNAS&v=2`)
+      .then((apiResponse) => {
+          resolve(apiResponse.data);
+          console.log("apiResponse.data in getBible",apiResponse.data);
       }).catch((error) => {
         reject(error);
       });  
@@ -22,5 +14,54 @@ app.factory("BibleFactory", function($http, $q, apiBible, FIREBASE_CONFIG) {
   };
 
   return {getBible:getBible};
+
 });
+// "use strict";
+
+// app.factory("DBPFactory", function($q, $http) {
+
+//     // This dam_id is pulling an NT english version of NAS New American Standard Bible
+//     var getBible = function() {
+//         return $q((resolve, reject) => {
+//             $http.get(`http://dbt.io/library/book?key=c0c769e931f78307a6c1c65cc5bd1d8c&dam_id=ENGNAS&v=2`)
+//             .success((response) => {
+//                 resolve(response);
+//             })
+//             .error((errorResponse) => {
+//                 reject(errorResponse);
+//             });
+//         });
+//     };
+
+//     var getChapters = function(book_id) {
+//         return $q((resolve, reject) => {
+//             $http.get(`http://dbt.io/library/chapter?key=c0c769e931f78307a6c1c65cc5bd1d8c&dam_id=ENGNAS&book_id=${book_id}&v=2`)
+//             .success((response) => {
+//                 resolve(response);
+//             })
+//             .error((errorResponse) => {
+//                 reject(errorResponse);
+//             });
+//         });
+//     };
+
+//     var getVerse = function(book_id, chapter_id, testament) {
+//         return $q((resolve, reject) => {
+//             $http.get(`http://dbt.io/text/verse?key=c0c769e931f78307a6c1c65cc5bd1d8c&dam_id=${testament}&book_id=${book_id}&chapter_id=${chapter_id}&v=2`)
+//             .success((response) => {
+//                 resolve(response);
+//             })
+//             .error((errorResponse) => {
+//                 reject(errorResponse);
+//             });
+//         });
+//     };
+
+//     return {
+//             getBible: getBible,
+//             getChapters: getChapters,
+//             getVerse: getVerse
+//         };
+
+// });  
 
