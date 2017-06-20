cbaeff451087c64e0ea6791356ea245e0e6b631c
bryanhmiller <bryankalos@hotmail.com>
2017-06-20 18:18:48 -0500

    Completed writing scripture_ref and scripture_url to firebase, Hallelujah!

--- javascripts/controllers/newNoteCtrl.js
+++ javascripts/controllers/newNoteCtrl.js
@@ -1,10 +1,19 @@
 app.controller("NewNoteCtrl", function($http, $location, $q, $rootScope, $routeParams, $scope, FIREBASE_CONFIG, NoteFactory) {
-
-  $scope.scripture_ref = `${$routeParams.book_name} ${$routeParams.chapter_id}: ${$routeParams.verse_id}`;
-
+  let bookName = $routeParams.book_name;
+  let bookId = $routeParams.book_id;
+  let chapterId = $routeParams.chapter_id;
+  let verseId = $routeParams.verse_id;
+  // console.log("$routeParams.book_name", $routeParams.book_name);
+  // console.log("$routeParams.book_name", $routeParams.book_name);
+  // console.log("$routeParams.book_name", $routeParams.book_name);
+  // console.log("$routeParams.book_name", $routeParams.book_name);
+  $scope.scripture_ref = `${bookName} ${chapterId}: ${verseId}`;
+  $scope.scripture_url = `${bookName}/${bookId}/${chapterId}/${verseId}`;  
+console.log("$scope.scripture_ref", $scope.scripture_ref);
+console.log("$scope.scripture_url", $scope.scripture_url);
   $scope.addNewNote = () => {
-    $scope.newNote.scripture_ref = scripture_ref;
-    $scope.newNote.scripture_url =`${routeParams.book_name}/${routeParams.book_id}/${routeParams.chapter_id}/${routeParams.verse_id}`;  
+    $scope.newNote.scripture_ref = $scope.scripture_ref;
+    $scope.newNote.scripture_url = $scope.scripture_url;
     $scope.newNote.is_personal = true;
     $scope.newNote.is_sermon = false;
     $scope.newNote.is_verses = false;
